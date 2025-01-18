bda1fd81d78aa32ad6b66206f97316e0786a3ea5
Varun Mahagaokar <varun090295@gmail.com>
2025-01-18 17:46:25 -0400

    removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/custom.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
 import NavbarTop from "./components/NavbarTop";
 import ESNavbar from "./components/Navbar";
 import KitchenBath from "./components/KitchenBath";
 import UniqueInstall from "./components/UniqueInstall";
-import NotFound from "./components/Partials/NotFound";
-import { Navbar } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
 import PlanningCare from "./components/PlanningCare";
 import OurServices from "./components/OurServices";
