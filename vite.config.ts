6ca5242fda2bbd53d6b85174e352b88dd5e51b56
Varun Mahagaokar <varun090295@gmail.com>
2025-03-18 19:38:57 -0300

    updated routing

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   server: {
-    historyApiFallback: true
+    open: true
+  },
+  preview: {
+    port: 4173
   }
 })
