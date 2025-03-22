b144d6800c944e02c23c1f9c518bfcbba458f9ae
Varun Mahagaokar <varun090295@gmail.com>
2025-03-22 01:06:31 -0300

    update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,101 +8,111 @@ import { HiMiniBars4 } from "react-icons/hi2";
 import { LiaTimesSolid } from "react-icons/lia";
 
 const ESNavbar = () => {
-	const [expanded, setExpanded] = useState(false);
-	const handleNavItemClick = () => {
-		setExpanded(false); // Close the navbar when a nav item is clicked
-	};
-	return (
-		<>
-			<Navbar className="es-navbar" expand="lg" expanded={expanded}>
-				<Navbar.Toggle
-					aria-controls="basic-navbar-nav"
-					className="mx-2"
-					onClick={() => setExpanded(expanded ? false : true)}>
-					{expanded ? (
-						<LiaTimesSolid size={30} color="white" />
-					) : (
-						<HiMiniBars4 size={30} color="white" />
-					)}
-				</Navbar.Toggle>
-				<Navbar.Collapse id="basic-navbar-nav">
-					<Container>
-						<Row>
-						<Nav className="w-100 justify-content-start d-lg-flex justify-content-between  align-items-lg-center">
-						{/* Render Nav.Link as React Router Link */}
-								<Nav.Link
-									as={NavLink}
-									to="/"
-									className="nav-text"
-									onClick={handleNavItemClick}>
-									Home
-								</Nav.Link>
-								<Nav.Link
-									as={NavLink}
-									to="/kitchenBath"
-									className="nav-text"
-									onClick={handleNavItemClick}>
-									Kitchen and Bath Design
-								</Nav.Link>
-								<Nav.Link
-									as={NavLink}
-									to="/uniqueInstall"
-									className="nav-text"
-									onClick={handleNavItemClick}>
-									Unique Installs
-								</Nav.Link>
-								<Nav.Link
-									as={NavLink}
-									to="/planningCare"
-									className="nav-text"
-									onClick={handleNavItemClick}>
-									Planning & Care
-								</Nav.Link>
-								<Nav.Link
-									as={NavLink}
-									to="/ourServices"
-									className="nav-text"
-									onClick={handleNavItemClick}>
-									Our Services
-								</Nav.Link>
-								<Nav.Link
-									as={NavLink}
-									to="/productPartners"
-									className="nav-text"
-									onClick={handleNavItemClick}>
-									Product Partners
-								</Nav.Link>
-								<Nav.Link
-									as={NavLink}
-									to="/contactUs"
-									className="nav-text"
-									onClick={handleNavItemClick}>
-									Contact Us
-								</Nav.Link>
-								<Nav.Link
-									as={NavLink}
-									to="/aboutUs"
-									className="nav-text"
-									onClick={handleNavItemClick}>
-									About Us
-								</Nav.Link>
-								
-								<Nav.Link
-									as={NavLink}
-									to="/contactUs"
-									onClick={handleNavItemClick}>
-									<Button className="mx-1" variant="btn btn-outline-light">
-										<CgNotes className="mx-1" />
-										Get Quote
-									</Button>
-								</Nav.Link>
-							</Nav>
-						</Row>
-					</Container>
-				</Navbar.Collapse>
-			</Navbar>
-		</>
-	);
+  const [expanded, setExpanded] = useState(false);
+  const handleNavItemClick = () => {
+    setExpanded(false); // Close the navbar when a nav item is clicked
+  };
+  return (
+    <>
+      <Navbar className="es-navbar" expand="lg" expanded={expanded}>
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          className="mx-2"
+          onClick={() => setExpanded(expanded ? false : true)}
+        >
+          {expanded ? (
+            <LiaTimesSolid size={30} color="white" />
+          ) : (
+            <HiMiniBars4 size={30} color="white" />
+          )}
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Container>
+            <Row>
+              <Nav className="w-100 justify-content-start d-lg-flex justify-content-between  align-items-lg-center">
+                {/* Render Nav.Link as React Router Link */}
+                <Nav.Link
+                  as={NavLink}
+                  to="/"
+                  className="nav-text"
+                  onClick={handleNavItemClick}
+                >
+                  Home
+                </Nav.Link>
+                <Nav.Link
+                  as={NavLink}
+                  to="/kitchenBath"
+                  className="nav-text"
+                  onClick={handleNavItemClick}
+                >
+                  Kitchen and Bath Design
+                </Nav.Link>
+                <Nav.Link
+                  as={NavLink}
+                  to="/uniqueInstall"
+                  className="nav-text"
+                  onClick={handleNavItemClick}
+                >
+                  Unique Installs
+                </Nav.Link>
+                <Nav.Link
+                  as={NavLink}
+                  to="/planningCare"
+                  className="nav-text"
+                  onClick={handleNavItemClick}
+                >
+                  Planning & Care
+                </Nav.Link>
+                <Nav.Link
+                  as={NavLink}
+                  to="/ourServices"
+                  className="nav-text"
+                  onClick={handleNavItemClick}
+                >
+                  Our Services
+                </Nav.Link>
+                <Nav.Link
+                  as={NavLink}
+                  to="/productPartners"
+                  className="nav-text"
+                  onClick={handleNavItemClick}
+                >
+                  Product Types
+                </Nav.Link>
+                <Nav.Link
+                  as={NavLink}
+                  to="/contactUs"
+                  className="nav-text"
+                  onClick={handleNavItemClick}
+                >
+                  Contact Us
+                </Nav.Link>
+                <Nav.Link
+                  as={NavLink}
+                  to="/aboutUs"
+                  className="nav-text"
+                  onClick={handleNavItemClick}
+                >
+                  About Us
+                </Nav.Link>
+
+                <Nav.Link
+                  as={NavLink}
+                  to="/contactUs"
+                  onClick={handleNavItemClick}
+                >
+                  <Button className="mx-1" variant="btn btn-outline-light">
+                    <CgNotes className="mx-1" />
+                    Get Quote
+                  </Button>
+                </Nav.Link>
+              </Nav>
+            </Row>
+          </Container>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
 };
 
 export default ESNavbar;
